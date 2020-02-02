c53ce89a1d7898f92138aa5f73b8554ef7537a89
Kriteeka Adhikari <krituadhikari@gmail.com>
2020-02-02 16:06:09 +0545

    added

---
+++ kriti/lab1 (1).py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Types are the building blocks upon which larger programs are made
+
+# Integer 
+
+# int Kriteekaadhikari - PASCAL CASE
+
+# kriteekaAdhikari - SNAKE CASE
+
+# int kriteeka_adhikari
+
+# Memory Heap
+# Mutability/Immutability
+
+# In[1]:
+
+
+4+5
+
+
+# In[2]:
+
+
+4 ** 2
+
+
+# In[4]:
+
+
+print("Hello World/n"*10)
+
+
+# 5%10-Mod
+
+# In[6]:
+
+
+1+2*3
+
+
+# In[7]:
+
+
+(1+2)*3
+
+
+# In[10]:
+
+
+4/3
+
+
+# In[12]:
+
+
+0.1+0.2-0.3
+
+
+# In[14]:
+
+
+any_word = "Hello World";
+any_word[0]
+any_word[-1]
+
+
+# In[15]:
+
+
+any_word[6]
+
+
+# In[23]:
+
+
+any_word[5]
+
+
+# In[16]:
+
+
+any_word[-5]
+
+
+# In[17]:
+
+
+any_word
+
+
+# In[18]:
+
+
+any_word[0:5]
+
+
+# In[19]:
+
+
+any_word[1:]
+
+
+# In[20]:
+
+
+any_word[0:11:2]
+
+
+# In[21]:
+
+
+any_word[::-1]
+
+
+# In[22]:
+
+
+any_word[::-2]
+
+
+# In[ ]:
+
+
+
+
